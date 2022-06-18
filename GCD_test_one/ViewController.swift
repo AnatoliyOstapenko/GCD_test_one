01ea24cf8dc957aa903f73226ceadc53f2d52d51
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-06-18 17:11:31 +0300

    Initial Commit

---
+++ GCD_test_one/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD_test_one
+//
+//  Created by AnatoliiOstapenko on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
